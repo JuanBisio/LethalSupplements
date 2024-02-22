341d24b11aee1ff2e5411a4f3f54174042b67096
bisiojuan07@gmail.com <bisiojuan@Juans-MacBook-Pro.local>
2024-02-21 22:39:30 -0300

    subimos el back

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 source .venv/bin/activate
 pip install --upgrade pip 
 pip install -r requirements.txt
-reflex init 
-reflex export --frontend-only 
 rm -rf public 
+reflex init 
+API_URL=https://https://lethalsupplements-production.up.railway.app reflex export --frontend-only 
 unzip frontend.zip -d public 
 rm -f frontend.zip 
 desactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="LethalSupplements",
-    api_url="https://lethalsupplements.store:8000",
 )
